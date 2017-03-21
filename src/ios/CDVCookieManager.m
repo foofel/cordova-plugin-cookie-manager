e933e3df4b7b6032944e77e47e42dadf8214415a
teunsteenbekkers <teun.steenbekkers@gmail.com>
2017-03-21 10:25:27 +0100

    Update and rename CDVCookies.m to CDVCookieManager.m

--- src/ios/CDVCookieManager.m
+++ src/ios/CDVCookieManager.m
@@ -23,9 +23,9 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#import "CDVCookies.h"
+#import "CDVCookieManager.h"
 
-@implementation CDVCookies
+@implementation CDVCookieManager
 
 - (void)clear:(CDVInvokedUrlCommand*)command
 {
