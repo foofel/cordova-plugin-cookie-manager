54c96f38fab067d6330055375c17d450cd95a3c0
teunsteenbekkers <teun.steenbekkers@gmail.com>
2017-03-21 09:33:10 +0100

    Update Cookies.java
    I copied the code changes made by fujunwei in a pull request for the original repository: https://github.com/bez4pieci/Phonegap-Cookies-Plugin/pull/7
    
    This makes the plugin compatible with Crosswalk WebViews.

--- src/android/Cookies.java
+++ src/android/Cookies.java
@@ -26,30 +26,31 @@
 
 package com.bez4pieci.cookies;
 
+import android.util.Log;
+import android.webkit.CookieManager;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import android.util.Log;
-import android.webkit.CookieManager;
+import java.lang.reflect.Method;
 
 public class Cookies extends CordovaPlugin {
-	
-	private final String TAG = "CookiesPlugin";
-	
-	@Override
+    private final String TAG = "CookiesPlugin";
+
+    @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if ("clear".equals(action)) {
             this.clear();
             callbackContext.success();
             return true;
         }
-        return false;  // Returning false results in a "MethodNotFound" error.
+        return false; // Returning false results in a "MethodNotFound" error.
     }
-	
-	public void clear() {
-		Log.v(TAG, "Clearing cookies...");
-        CookieManager.getInstance().removeAllCookies(null);
+
+    public void clear() {
+        Log.v(TAG, "Clearing cookies...");
+        webView.getCookieManager().clearCookies();
     }
 }
