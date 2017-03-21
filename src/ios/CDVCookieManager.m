9f7728e1c712300b0245b47a8d27a05c8f3f1c05
teunsteenbekkers <teun.steenbekkers@gmail.com>
2017-03-21 10:26:16 +0100

    Update CDVCookieManager.m
    Change indentation to 2 spaces.

--- src/ios/CDVCookieManager.m
+++ src/ios/CDVCookieManager.m
@@ -29,15 +29,15 @@
 
 - (void)clear:(CDVInvokedUrlCommand*)command
 {
-    NSHTTPCookie *cookie;
-    NSHTTPCookieStorage *storage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
-    for (cookie in [storage cookies]) {
-        [storage deleteCookie:cookie];
-    }
-    [[NSUserDefaults standardUserDefaults] synchronize];
+  NSHTTPCookie *cookie;
+  NSHTTPCookieStorage *storage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+  for (cookie in [storage cookies]) {
+    [storage deleteCookie:cookie];
+  }
+  [[NSUserDefaults standardUserDefaults] synchronize];
     
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+  CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
